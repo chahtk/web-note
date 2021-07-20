a562ae965d0a47abc10edbadb1c1a15a93e88a80
chahtk <44664867+chahtk@users.noreply.github.com>
2021-07-20 22:10:46 +0900

    Refactor take out function

--- client/src/components/calendar/Calendar.js
+++ client/src/components/calendar/Calendar.js
@@ -11,6 +11,38 @@ const YearMonthStyle = styled.div`
   text-align: center;
 `;
 
+const increaseMonth = (ym, setYm) => () => {
+  const { year, month } = ym;
+  if (month + 1 === 13) {
+    setYm({
+      ...ym,
+      year: year + 1,
+      month: 1,
+    });
+    return;
+  }
+  setYm({
+    ...ym,
+    month: month + 1,
+  });
+};
+
+const decreaseMonth = (ym, setYm) => () => {
+  const { year, month } = ym;
+  if (month - 1 === 0) {
+    setYm({
+      ...ym,
+      year: year - 1,
+      month: 12,
+    });
+    return;
+  }
+  setYm({
+    ...ym,
+    month: month - 1,
+  });
+};
+
 const Calendar = () => {
   const date = new Date();
   const [ym, setYm] = useState({
@@ -18,45 +50,13 @@ const Calendar = () => {
     month: date.getMonth(),
   });
 
-  const increaseMonth = () => {
-    const { year, month } = ym;
-    if (month + 1 === 13) {
-      setYm({
-        ...ym,
-        year: year + 1,
-        month: 1,
-      });
-      return;
-    }
-    setYm({
-      ...ym,
-      month: month + 1,
-    });
-  };
-
-  const decreaseMonth = () => {
-    const { year, month } = ym;
-    if (month - 1 === 0) {
-      setYm({
-        ...ym,
-        year: year - 1,
-        month: 12,
-      });
-      return;
-    }
-    setYm({
-      ...ym,
-      month: month - 1,
-    });
-  };
-
   return (
     <div>
-      <ButtonStyle onClick={decreaseMonth}>&lt;</ButtonStyle>
+      <ButtonStyle onClick={decreaseMonth(ym, setYm)}>&lt;</ButtonStyle>
       <YearMonthStyle>
         {ym.year}년 {ym.month}월
       </YearMonthStyle>
-      <ButtonStyle onClick={increaseMonth}>&gt;</ButtonStyle>
+      <ButtonStyle onClick={increaseMonth(ym, setYm)}>&gt;</ButtonStyle>
     </div>
   );
 };
