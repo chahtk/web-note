75558dc5e0dbc666bb414433965c99f6d7b4db96
chahtk <44664867+chahtk@users.noreply.github.com>
2021-07-20 22:02:47 +0900

    Feat add header component

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderStyle = styled.div`
+  height: 50px;
+  background-color: grey;
+`;
+
+const Header = () => {
+  return <HeaderStyle />;
+};
+
+export default Header;
