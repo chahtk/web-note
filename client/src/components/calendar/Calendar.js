4508539517265d1afee092077c2540b21214bf89
chahtk <44664867+chahtk@users.noreply.github.com>
2021-07-22 19:50:13 +0900

    Refactor set apart component

--- client/src/components/calendar/Calendar.js
+++ client/src/components/calendar/Calendar.js
@@ -1,43 +1,9 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import CalendarHeader from './CalendarHeader';
-
-const TableStyle = styled.table`
-  border: 1px solid grey;
-  border-collapse: collapse;
-`;
-
-const getCalendarArray = (ym) => {
-  const firstDay = new Date(ym.year, ym.month, 1).getDay();
-  const lastDate = new Date(ym.year, ym.month + 1, 0).getDate();
-  const calendarArray = [];
-  for (let i = 0; i < 7; i += 1) {
-    if (i !== firstDay) {
-      calendarArray.push(0); // prev month's day
-    } else break;
-  }
-  for (let i = 1; i <= lastDate; i += 1) calendarArray.push(i);
-  return calendarArray;
-};
-
-const renderCalendar = (ym) => {
-  const calendarArray = getCalendarArray(ym);
-  const rows = Math.floor((calendarArray.length - 1) / 7) + 1;
-  const calendar = [];
-  for (let i = 0; i < rows; i += 1) {
-    const row = [];
-    for (let j = 0; j < 7; j += 1) {
-      const day = calendarArray[i * 7 + j];
-      row.push(<td key={`cell${i}${j}`}>{day === 0 ? null : day}</td>);
-    }
-    calendar.push(<tr key={`asf${i}`}>{row}</tr>);
-  }
-  return calendar;
-};
+import CalendarBody from './CalendarBody';
 
 const Calendar = () => {
   const date = new Date();
-  const days = ['일', '월', '화', '수', '목', '금', '토'];
   const [ym, setYm] = useState({
     year: date.getFullYear(),
     month: date.getMonth(),
@@ -46,18 +12,7 @@ const Calendar = () => {
   return (
     <div id="calendar">
       <CalendarHeader ym={ym} setYm={setYm} />
-      <div id="calendar-body">
-        <TableStyle>
-          <thead>
-            <tr>
-              {days.map((day) => (
-                <th key={`dayIs${day}`}>{day}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>{renderCalendar(ym)}</tbody>
-        </TableStyle>
-      </div>
+      <CalendarBody ym={ym} />
     </div>
   );
 };
---
+++ client/src/components/calendar/CalendarBody.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TableStyle = styled.table`
+  border: 1px solid grey;
+  border-collapse: collapse;
+`;
+
+const getCalendarArray = (ym) => {
+  const firstDay = new Date(ym.year, ym.month, 1).getDay();
+  const lastDate = new Date(ym.year, ym.month + 1, 0).getDate();
+  const calendarArray = [];
+  for (let i = 0; i < 7; i += 1) {
+    if (i !== firstDay) {
+      calendarArray.push(0); // prev month's day
+    } else break;
+  }
+  for (let i = 1; i <= lastDate; i += 1) calendarArray.push(i);
+  return calendarArray;
+};
+
+const renderCalendar = (ym) => {
+  const calendarArray = getCalendarArray(ym);
+  const rows = Math.floor((calendarArray.length - 1) / 7) + 1;
+  const calendar = [];
+  for (let i = 0; i < rows; i += 1) {
+    const row = [];
+    for (let j = 0; j < 7; j += 1) {
+      const day = calendarArray[i * 7 + j];
+      row.push(<td key={`cell${i}${j}`}>{day === 0 ? null : day}</td>);
+    }
+    calendar.push(<tr key={`asf${i}`}>{row}</tr>);
+  }
+  return calendar;
+};
+
+const CalendarBody = ({ ym }) => {
+  /*
+    const [ym, setYm] = useState({
+      year: date.getFullYear(),
+      month: date.getMonth(),
+    });
+  */
+  const days = ['일', '월', '화', '수', '목', '금', '토'];
+
+  return (
+    <div id="calendar-body">
+      <TableStyle>
+        <thead>
+          <tr>
+            {days.map((day) => (
+              <th key={`dayIs${day}`}>{day}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>{renderCalendar(ym)}</tbody>
+      </TableStyle>
+    </div>
+  );
+};
+
+export default CalendarBody;
