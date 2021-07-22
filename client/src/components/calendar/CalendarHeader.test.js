a24e9cc11d12d25a0acd9b7898ff023d162fce29
chahtk <44664867+chahtk@users.noreply.github.com>
2021-07-22 22:41:38 +0900

    Style change code formatting

--- client/src/components/calendar/CalendarHeader.test.js
+++ client/src/components/calendar/CalendarHeader.test.js
@@ -4,43 +4,45 @@ import '@testing-library/jest-dom';
 import Calendar from './Calendar';
 
 describe('Calendar Buttons', () => {
-  
-  const getYearMonthElement = (calendar) => 
+  const getYearMonthElement = (calendar) =>
     calendar.getByText(/\d{4}년 \d{1,2}월/i);
 
   const getYearMonth = (yearMonth) => {
     const { textContent } = yearMonth;
-    const [year, month] = textContent.split(' ')
-    return [+year.slice(0, -1), +month.slice(0, -1)];
+    const [, month] = textContent.split(' ');
+    return +month.slice(0, -1);
   };
 
-  let date, calendar, yearMonth, thisMonth;
+  let date;
+  let calendar;
+  let yearMonth;
+  let thisMonth;
 
   const init = () => {
     date = new Date();
     calendar = render(<Calendar />);
     yearMonth = getYearMonthElement(calendar);
     thisMonth = date.getMonth() + 1;
-  }
+  };
 
-  beforeEach(() => init())
+  beforeEach(() => init());
 
   test('Default Month', () => {
-    const [year, month] = getYearMonth(yearMonth);
+    const month = getYearMonth(yearMonth);
     expect(month).toEqual(thisMonth);
-  })
+  });
 
   test('Add Month', () => {
     const plusButton = calendar.getByText('>');
     fireEvent.click(plusButton);
-    const [year, month] = getYearMonth(yearMonth);
+    const month = getYearMonth(yearMonth);
     expect(month).toEqual(thisMonth + 1);
-  })
+  });
 
   test('Minus Month', () => {
     const minusButton = calendar.getByText('<');
     fireEvent.click(minusButton);
-    const [year, month] = getYearMonth(yearMonth);
+    const month = getYearMonth(yearMonth);
     expect(month).toEqual(thisMonth - 1);
-  })
+  });
 });
