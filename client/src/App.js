167c980e182d97dd64a502ea0c2812757004ebb9
chahtk <44664867+chahtk@users.noreply.github.com>
2021-07-24 05:48:39 +0900

    Feat add state about sign modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Calendar from './components/calendar/Calendar';
 import Header from './components/header/Header';
+import SignModal from './components/sign/SignModal';
 
 const App = () => {
+  const [signModal, setSignModal] = useState(false);
   return (
     <>
-      <Header />
-      <Calendar />
+      <Header setSignModal={setSignModal} />
+      {signModal ? <SignModal setSignModal={setSignModal} /> : null}
+      {/* <Calendar /> */}
     </>
   );
 };
