beb65f06af7cd8e51c1cce9a1ba20cc2038cc1a9
chahtk <44664867+chahtk@users.noreply.github.com>
2021-07-24 05:49:05 +0900

    Feat add login component

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
+import SignInButton from '../sign/SignInButton';
 
-const HeaderStyle = styled.div`
-  height: 50px;
-  background-color: grey;
+const HeaderStyle = styled.header`
+  height: 70px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  background-color: inherit;
+  border-bottom: 1px solid grey;
 `;
 
-const Header = () => {
-  return <HeaderStyle />;
+const RightMenu = styled.div`
+  display: flex;
+  float: right;
+  height: 100%;
+  align-items: center;
+`;
+
+const Header = ({ setSignModal }) => {
+  return (
+    <HeaderStyle>
+      <RightMenu>
+        <SignInButton setSignModal={setSignModal} />
+      </RightMenu>
+    </HeaderStyle>
+  );
 };
 
 export default Header;
